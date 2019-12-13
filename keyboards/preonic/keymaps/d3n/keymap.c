dcb21de683c42824003ac11e9ae746f9a106f53f
Dowon Kang <dowon@tuta.io>
2019-12-13 14:23:37 +0900

    Daily keymap adjustment

--- keyboards/preonic/keymaps/d3n/keymap.c
+++ keyboards/preonic/keymaps/d3n/keymap.c
@@ -47,6 +47,7 @@ enum preonic_keycodes {
 #define MEN_ALT RALT_T(KC_APPLICATION)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // clang-format off
 
     /* Qwerty
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -62,14 +63,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_QWERTY] = LAYOUT_preonic_grid(
-        // clang-format off
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
         ESCTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SFT, \
         NUMPAD,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   ENT_LOW, SPC_RSE, RAISE,   MEN_ALT, LEFT,    DOWN,    RIGHT
-        // clang-format on
-        ),
+    ),
 
     /* Arrows
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -85,14 +84,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_ARROWS] = LAYOUT_preonic_grid(
-        // clang-format off
         DEFAULT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
-        // clang-format on
-        ),
+    ),
 
     /* Lower
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -108,14 +105,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_LOWER] = LAYOUT_preonic_grid(
-        // clang-format off
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
         _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______, \
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT
-        // clang-format on
-        ),
+    ),
 
     /* Raise
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -131,14 +126,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_RAISE] = LAYOUT_preonic_grid(
-        // clang-format off
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, \
         _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_INS,  KC_HOME, KC_PGUP, _______, _______, \
         KC_CAPS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_DEL,  KC_END,  KC_PGDN, _______, _______, \
         _______, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-        // clang-format on
-        ),
+    ),
 
     /* Numpad
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -154,14 +147,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_NUMPAD] = LAYOUT_preonic_grid(
-        // clang-format off
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PEQL, \
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P0,   XXXXXXX, KC_PDOT, XXXXXXX
-        // clang-format on
-        ),
+    ),
 
     /* Adjust (Lower + Raise)
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
@@ -177,15 +168,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_ADJUST] = LAYOUT_preonic_grid(
-        // clang-format off
         _______, RESET,   DEBUG,   _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, AU_TOG,  _______, _______, _______, _______, _______, MU_MOD,  _______, _______, _______, _______, \
         _______, MUV_DE,  MUV_IN,  _______, _______, _______, _______, MU_TOG,  MI_TOG,  _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-        // clang-format on
-        )
-
+    )
+    // clang-format on
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -193,9 +182,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case DEFAULT:
             if (record->event.pressed) {
                 layer_move(_QWERTY);
-                register_code(KC_ESCAPE);
-            } else {
-                unregister_code(KC_ESCAPE);
             }
             return false;
             break;
