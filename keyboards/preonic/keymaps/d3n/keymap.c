429d18c4159a7c31ab9fcec77e9ef4c46c1e5343
Dowon Kang <dowon@tuta.io>
2019-12-18 18:23:40 +0900

    Swap Lower/Raise keys and Enter/Space keys
    Why:
    
    * Feels more natural to me.

--- keyboards/preonic/keymaps/d3n/keymap.c
+++ keyboards/preonic/keymaps/d3n/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │ Shift │   Z   │   X   │   C   │   V   │   B   │   N   │   M   │   ,   │   .   │   /   │ENT/SFT│
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ Numpad│ Ctrl  │  GUI  │  Alt  │ Lower │ Enter │ Space │ Raise │Mn/Alt │ Left  │ Down  │ Right │
+     * │ Numpad│ Ctrl  │  GUI  │  Alt  │ Enter │ Lower │ Raise │ Space │Mn/Alt │ Left  │ Down  │ Right │
      * └───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_QWERTY] = LAYOUT_preonic_grid(
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
         ESCTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SFT, \
-        NUMPAD,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   ENT_LOW, SPC_RSE, RAISE,   MEN_ALT, LEFT,    DOWN,    RIGHT
+        NUMPAD,  KC_LCTL, KC_LGUI, KC_LALT, ENT_LOW, LOWER,   RAISE,   SPC_RSE, MEN_ALT, LEFT,    DOWN,    RIGHT
     ),
 
     /* Arrows
