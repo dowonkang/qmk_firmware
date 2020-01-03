05010ac34860043c78e3a201a0614e99eb623875
Dowon Kang <dowon@tuta.io>
2020-01-03 13:07:33 +0900

    ESC becomes ` when pressed with Alt
    Why:
    
    * To make it easier to switch between same app windows

--- keyboards/preonic/keymaps/d3n/keymap.c
+++ keyboards/preonic/keymaps/d3n/keymap.c
@@ -28,13 +28,26 @@ enum preonic_layers {
     // clang-format on
 };
 
+enum preonic_keycodes {
+    // clang-format off
+    ALT_ESC = SAFE_RANGE
+    // clang-format on
+};
+
 // Let's dance
 typedef struct {
     bool is_press_action;
     int  state;
 } tap;
 
-enum { SINGLE_TAP = 1, SINGLE_HOLD = 2, DOUBLE_TAP = 3, DOUBLE_HOLD = 4 };
+enum {
+    // clang-format off
+    SINGLE_TAP = 1,
+    SINGLE_HOLD = 2,
+    DOUBLE_TAP = 3,
+    DOUBLE_HOLD = 4
+    // clang-format on
+};
 
 enum { EXTRA = 0 };
 
@@ -64,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────────────┴───────────────┴───────┴───────┴───────┴───────┘
      */
     [_QWERTY] = LAYOUT_preonic_2x2u(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+        ALT_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
@@ -177,6 +190,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
     // clang-format on
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case ALT_ESC: {
+            static uint8_t kc;
+            static bool    is_alt_pressed;
+
+            if (record->event.pressed) {
+                is_alt_pressed = get_mods() & MOD_MASK_ALT;
+
+                if (is_alt_pressed) {
+                    kc = KC_GRAVE;
+                } else {
+                    kc = KC_ESCAPE;
+                }
+
+                register_code(kc);
+            } else {
+                unregister_code(kc);
+            }
+            break;
+        }
+    }
+    return true;
+}
 
 uint32_t layer_state_set_user(uint32_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
 
