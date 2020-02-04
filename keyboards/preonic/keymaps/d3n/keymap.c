f32a70f00a24efd664b4c2f2ab0610a413f5b8eb
Dowon Kang <dowon@tuta.io>
2020-02-04 09:34:03 +0900

    Changed some modifier key locations

--- keyboards/preonic/keymaps/d3n/keymap.c
+++ keyboards/preonic/keymaps/d3n/keymap.c
@@ -19,7 +19,7 @@
 
 enum preonic_layers {
     // clang-format off
-    _QWERTY,
+    _BASE,
     _LOWER,
     _RAISE,
     _NUMPAD,
@@ -66,25 +66,25 @@ void extra_reset(qk_tap_dance_state_t *state, void *user_data);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
 
-    /* Qwerty
+    /* Base
      * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
      * │ Esc   │   1   │   2   │   3   │   4   │   5   │   6   │   7   │   8   │   9   │   0   │   `   │
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │ Tab   │   Q   │   W   │   E   │   R   │   T   │   Y   │   U   │   I   │   O   │   P   │BackSpc│
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ Esc   │   A   │   S   │   D   │   F   │   G   │   H   │   J   │   K   │   L   │   ;   │   '   │
+     * │ Ctrl  │   A   │   S   │   D   │   F   │   G   │   H   │   J   │   K   │   L   │   ;   │   '   │
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │ Shift │   Z   │   X   │   C   │   V   │   B   │   N   │   M   │   ,   │   .   │   /   │Ent/Sft│
      * ├───────┼───────┼───────┼───────┼───────┴───────┼───────┴───────┼───────┼───────┼───────┼───────┤
-     * │ DANCE │ GUI   │  Alt  │ Ctrl  │  Space/Lower  │  Raise/Space  │ Left  │ Down  │  Up   │ Right │
+     * │ ROTARY│ DANCE │  Alt  │ Super │  Space/Lower  │  Raise/Space  │ Left  │ Down  │  Up   │ Right │
      * └───────┴───────┴───────┴───────┴───────────────┴───────────────┴───────┴───────┴───────┴───────┘
      */
-    [_QWERTY] = LAYOUT_preonic_2x2u(
+    [_BASE] = LAYOUT_preonic_2x2u(
         GRAVESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
-        DANCE,   KC_LGUI, KC_LALT, KC_LCTL,          LOW_SPC, RSE_SPC,          CTL_LFT, ALT_DN,  KC_UP,   CTL_RGT
+        _______, DANCE,   KC_LALT, KC_LGUI,          LOW_SPC, RSE_SPC,          CTL_LFT, ALT_DN,  KC_UP,   CTL_RGT
     ),
 
     /* Lower
