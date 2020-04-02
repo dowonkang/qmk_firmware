35983bdc84dadf52fb15c7e997f20426772ac2ca
Dowon Kang <dowon@tuta.io>
2020-04-02 18:02:07 +0900

    Keymap update

--- keyboards/preonic/keymaps/d3n/keymap.c
+++ keyboards/preonic/keymaps/d3n/keymap.c
@@ -22,6 +22,7 @@ enum preonic_layers {
     _BASE,
     _LOWER,
     _RAISE,
+    _WEB,
     _NUMPAD,
     _ADJUST
     // clang-format on
@@ -29,6 +30,7 @@ enum preonic_layers {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define FN_ESC LT(_WEB, KC_ESC)
 #define CTL_LFT CTL_T(KC_LEFT)
 #define ALT_DN ALT_T(KC_DOWN)
 #define GUI_UP GUI_T(KC_UP)
@@ -54,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_preonic_1x2uC(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+        FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
         NUMPAD,  KC_LGUI, KC_LALT, KC_LCTL, LOWER,       KC_SPC,       RAISE,   CTL_LFT, ALT_DN,  GUI_UP,  CTL_RGT
     ),
@@ -88,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
      * │       │  F5   │  F6   │  F7   │  F8   │       │ Left  │ Down  │  Up   │ Right │       │       │
      * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-     * │       │  F9   │  F10  │  F11  │  F12  │       │       │       │       │       │       │       │
+     * │       │  F9   │  F10  │  F11  │  F12  │       │       │       │       │       │       │ Enter │
      * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
      * │       │       │       │       │       │               │       │       │       │ MENU  │       │
      * └───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────┘
@@ -97,8 +99,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PSCR, \
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, \
         _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, KC_ENT, \
         _______, _______, _______, _______, _______,      _______,     _______, _______, _______, KC_APP,  _______
+    ),
+
+    /* Web
+     * ┌───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┬───────┐
+     * │       │       │       │       │       │       │       │       │       │       │       │       │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │       │       │ Home  │       │       │       │       │       │       │       │       │       │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │       │ Back  │  End  │Forward│       │       │       │       │       │       │       │       │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┼───────┤
+     * │       │       │       │       │       │       │       │       │       │       │       │       │
+     * ├───────┼───────┼───────┼───────┼───────┼───────┴───────┼───────┼───────┼───────┼───────┼───────┤
+     * │       │       │       │       │       │               │       │       │       │       │       │
+     * └───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────┘
+     */
+    [_WEB] = LAYOUT_preonic_1x2uC(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, KC_HOME, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, KC_WBAK, KC_END,  KC_WFWD, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
     ),
 
     /* Numpad & Mouse keys
@@ -115,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───────┴───────┴───────┴───────┴───────┴───────────────┴───────┴───────┴───────┴───────┴───────┘
      */
     [_NUMPAD] = LAYOUT_preonic_1x2uC(
-        XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______, \
+        XXXXXXX, XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, XXXXXXX, \
         _______, XXXXXXX, KC_WH_L, KC_MS_U, KC_WH_R, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______, \
         _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PEQL, _______, \
         _______, XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, \
@@ -158,8 +181,10 @@ bool led_update_user(led_t led_state) {
 uint32_t layer_state_set_user(uint32_t state) {
     if (layer_state_is(_NUMPAD) && !numlock_state) {
         tap_code(KC_NUMLOCK);
+        numlock_state = true;
     } else if (numlock_state) {
         tap_code(KC_NUMLOCK);
+        numlock_state = false;
     }
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
@@ -186,7 +211,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
             }
         }
     } else {
-        if (IS_LAYER_ON(_RAISE)) {
+        if (IS_LAYER_ON(_RAISE) || IS_LAYER_ON(_NUMPAD)) {
             if (clockwise) {
                 tap_code(KC_MS_WH_DOWN);
             } else {
