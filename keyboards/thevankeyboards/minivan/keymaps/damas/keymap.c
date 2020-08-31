5814ed818e12859985249db4baf81def8ca38f77
Dowon Kang <dowonkang@protonmail.com>
2020-08-31 22:39:46 +0900

    Replace UNLOCK to BASE

--- keyboards/thevankeyboards/minivan/keymaps/damas/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/damas/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_HOME, KC_UP  , KC_END , KC_VOLU, XXXXXXX, KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, KC_BSPC,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, XXXXXXX, KC_TAB , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, XXXXXXX, XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,
-        _______, _______, _______,          KC_SPC          ,          KC_P0           , KC_P0  , KC_PDOT, UNLOCK
+        _______, _______, _______,          KC_SPC          ,          KC_P0           , KC_P0  , KC_PDOT, BASE
     ),
     [_FUNCTION] = LAYOUT(
         _______, KC_WBAK, KC_HOME, KC_PGUP, KC_VOLU, KC_PSCR, XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,
