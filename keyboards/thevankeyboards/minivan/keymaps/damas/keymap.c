771520d7871643194e796e3cea11b58c23b93b5f
Dowon Kang <dowonkang@protonmail.com>
2020-12-21 16:15:39 +0900

    New keymap for my Minivan

--- keyboards/thevankeyboards/minivan/keymaps/damas/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/damas/keymap.c
@@ -6,21 +6,19 @@ enum damas_layers {
     // clang-format off
     _BASE,
     _SYMBOLS,
-    _NUMPAD,
-    _NUMPADLOCK,
-    _FUNCTION,
-    _FUNCTIONLOCK,
+    _ARROW_FUNCTION,
+    _NAV_NUMPAD,
+    _MOUSE,
     _ADJUST
     // clang-format on
 };
 
 // Shortcuts
-#define FN_TAB LT(_FUNCTION, KC_TAB)
-#define FN_ESC LT(_NUMPAD, KC_ESCAPE)
-#define BASE TO(_BASE)
+#define FN_TAB LT(_NAV_NUMPAD, KC_TAB)
+#define FN_ESC LT(_ARROW_FUNCTION, KC_ESCAPE)
 #define SYMBOLS MO(_SYMBOLS)
-#define LOCK_NP TO(_NUMPADLOCK)
-#define LOCK_FN TO(_FUNCTIONLOCK)
+#define TO_BASE TO(_BASE)
+#define TO_MOUS TO(_MOUSE)
 #define CTL_A LCTL(KC_A)
 #define CTL_S LCTL(KC_S)
 #define CTL_D LCTL(KC_D)
@@ -46,29 +44,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, CTL_Z  , CTL_X  , CTL_C  , CTL_V  , CTL_B  , KC_BSLS, KC_EQL , _______, _______, _______, _______,
         _______, _______, _______,          _______         ,          _______         , _______, _______, _______
     ),
-    [_NUMPAD] = LAYOUT(
-        _______, KC_HOME, KC_UP  , KC_END , KC_VOLU, XXXXXXX, KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, KC_BSPC,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, XXXXXXX, KC_TAB , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, XXXXXXX, XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,
-        _______, _______, _______,          KC_SPC          ,          KC_P0           , KC_P0  , KC_PDOT, LOCK_NP
-    ),
-    [_NUMPADLOCK] = LAYOUT(
-        _______, KC_HOME, KC_UP  , KC_END , KC_VOLU, XXXXXXX, KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, KC_BSPC,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, XXXXXXX, KC_TAB , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, XXXXXXX, XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,
-        _______, _______, _______,          KC_SPC          ,          KC_P0           , KC_P0  , KC_PDOT, BASE
-    ),
-    [_FUNCTION] = LAYOUT(
-        _______, KC_WBAK, KC_HOME, KC_PGUP, KC_VOLU, KC_PSCR, XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,
-        _______, KC_WFWD, KC_END , KC_PGDN, KC_VOLD, KC_SLCK, XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
+    [_ARROW_FUNCTION] = LAYOUT(
+        TO_MOUS, KC_HOME, KC_UP  , KC_END , KC_VOLU, KC_PSCR, XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_SLCK, XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PAUS, XXXXXXX, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
-        _______, _______, _______,          KC_SPC          ,          _______         , XXXXXXX, _______, LOCK_FN
+        _______, _______, _______,          KC_SPC          ,          _______         , _______, _______, _______
     ),
-    [_FUNCTIONLOCK] = LAYOUT(
-        _______, KC_WBAK, KC_HOME, KC_PGUP, KC_VOLU, KC_PSCR, XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,
-        _______, KC_WFWD, KC_END , KC_PGDN, KC_VOLD, KC_SLCK, XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PAUS, XXXXXXX, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
-        _______, _______, _______,          KC_SPC          ,          _______         , XXXXXXX, _______, BASE
+    [_NAV_NUMPAD] = LAYOUT(
+        _______, KC_WBAK, KC_HOME, KC_PGUP, KC_VOLU, KC_PSCR, KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______,
+        _______, KC_WFWD, KC_END , KC_PGDN, KC_VOLD, KC_SLCK, KC_TAB , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PAUS, XXXXXXX, KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,
+        _______, _______, _______,          KC_SPC          ,          KC_P0           , KC_P0  , KC_PDOT, _______
+    ),
+    [_MOUSE] = LAYOUT(
+        TO_BASE, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
+        TO_BASE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______, _______, _______,          KC_BTN1         ,          KC_BTN2         , KC_BTN3, XXXXXXX, XXXXXXX
     ),
     [_ADJUST] = LAYOUT(
         RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -103,18 +95,22 @@ uint32_t layer_state_set_user(uint32_t state) {
         led0r = 255;
         led0b = 255;
     }
+    if (layer_state_cmp(state, _MOUSE)) {
+        led0r = 255;
+        led0g = 255;
+    }
     if (layer_state_cmp(state, _SYMBOLS)) {
         led0b = 255;
     }
-    if (layer_state_cmp(state, _NUMPAD)) {
-        led0g = 255;
-    }
-    if (layer_state_cmp(state, _FUNCTION)) {
+    if (layer_state_cmp(state, _ARROW_FUNCTION)) {
         led0r = 255;
+    }
+    if (layer_state_cmp(state, _NAV_NUMPAD)) {
+        led0g = 255;
     }
     setrgb(led0r, led0g, led0b, (LED_TYPE *)&led[0]);
 #endif  // RGBLIGHT_ENABLE
-    return update_tri_layer_state(state, _NUMPAD, _SYMBOLS, _ADJUST);
+    return update_tri_layer_state(state, _ARROW_FUNCTION, _SYMBOLS, _ADJUST);
 }
 
 bool led_update_user(led_t led_state) {
