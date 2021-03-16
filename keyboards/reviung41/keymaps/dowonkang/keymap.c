c029d9ce7466fd3c154d9e3e62f2c9af41490fa2
Dowon Kang <dev@dowonkang.name>
2021-03-16 15:57:26 +0900

    Adjust home-mods

--- keyboards/reviung41/keymaps/dowonkang/keymap.c
+++ keyboards/reviung41/keymaps/dowonkang/keymap.c
@@ -19,37 +19,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [_BASE] = LAYOUT_reviung41(
         FN_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,          KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
-        U_ESC  , HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,          KC_H   , HOME_J , HOME_K , HOME_L , HOME_SC, KC_ENT ,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,          KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+        U_ESC  , H_A    , H_S    , H_D    , H_F    , KC_G   ,          KC_H   , H_J    , H_K    , H_L    , H_SCLN , KC_ENT ,
+        KC_LSFT, KC_Z   , H_X    , KC_C   , KC_V   , KC_B   ,          KC_N   , KC_M   , KC_COMM, H_DOT  , KC_SLSH, KC_RSFT,
                                    KC_LALT, LOWER  ,          U_SPACE,          RAISE  , KC_LGUI
     ),
 
     [_LOWER] = LAYOUT_reviung41(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_DEL ,
-        KC_ESC , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,          KC_QUOT, LOW_J  , LOW_K  , LOW_L  , _______, _______,
+        KC_ESC , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_GRV ,          KC_QUOT, LOW_J  , LOW_K  , LOW_L  , _______, _______,
         _______, KC_APP , KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,          KC_BSLS, KC_EQL , _______, _______, _______, _______,
                                    _______, _______,          KC_SPC ,          _______, _______
     ),
 
     [_RAISE] = LAYOUT_reviung41(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,          KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_DEL ,
-        KC_ESC , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, _______,
+        KC_ESC , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_GRV ,          KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, _______,
         _______, KC_APP , KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,          KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, _______,
                                    _______, _______,          _______,          _______, KC_APP
     ),
 
     [_FUNCTION] = LAYOUT_reviung41(
-        _______, KC_WBAK, KC_HOME, KC_WFWD, KC_WREF, KC_VOLU,          KC_PSCR, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_INS ,
-        KC_ESC , XXXXXXX, KC_END , KC_WSCH, KC_WSTP, KC_VOLD,          KC_SLCK, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_WHOM, KC_MUTE,          KC_PAUS, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_VOLU,          KC_PSCR, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_INS ,
+        KC_ESC , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_VOLD,          KC_SLCK, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
+        _______, KC_APP , KC_RALT, XXXXXXX, XXXXXXX, KC_MUTE,          KC_PAUS, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
                                    _______, _______,          _______,          _______, _______
     ),
 
     [_NUMPAD] = LAYOUT_reviung41(
-        _______, KC_HOME, KC_UP  , KC_END , KC_PGUP, XXXXXXX,          KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______,
-        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,          KC_TAB , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_PENT,
+        _______, WEB_BCK, KC_UP  , WEB_FWD, KC_HOME, KC_PGUP,          KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END , KC_PGDN,          KC_CAPS, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_PENT,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_PAST, KC_P1  , KC_P2  , KC_P3  , KC_PSLS, _______,
-                                   _______, KC_LGUI,          KC_SPC ,          KC_P0  , KC_PDOT
+                                   _______, KC_LCTL,          KC_SPC ,          KC_P0  , KC_PDOT
     ),
 
     [_CTRL] = LAYOUT_reviung41(
@@ -59,13 +59,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, CTRL_Z , CTRL_X , CTRL_C , CTRL_V , CTRL_B ,          _______, _______, _______, _______, _______, _______,
                                    _______, _______,          _______,          _______, _______
         // clang-format on
-    ),
+        ),
 
     [_ADJUST] = LAYOUT_reviung41(
+        // clang-format off
         RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_CLCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                    _______, _______,          XXXXXXX,          _______, _______
-    ),
-    // clang-format on
+        // clang-format on
+        ),
 };
--- users/dowonkang/dowonkang.c
+++ users/dowonkang/dowonkang.c
@@ -12,6 +12,9 @@ __attribute__((weak)) bool process_record_keymap(uint16_t keycode, keyrecord_t *
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint16_t u_esc_timer;
     static uint16_t u_space_timer;
+    static uint16_t low_esc_timer;
+    static uint16_t num_tab_timer;
+    static uint16_t fn_bspc_timer;
     switch (keycode) {
         case U_ESC:
             if (record->event.pressed) {
@@ -35,8 +38,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_on(_CTRL);
             } else {
                 layer_off(_CTRL);
-                if (timer_elapsed(u_space_timer) < TAPPING_TERM) {
+                if (timer_elapsed(u_space_timer) < TAPPING_TERM + 50) {
                     tap_code(KC_SPACE);
+                }
+            }
+            return false;
+        case LOW_ESC:
+            if (record->event.pressed) {
+                low_esc_timer = timer_read();
+                layer_on(_LOWER);
+            } else {
+                uint8_t mods = get_mods();
+                layer_off(_LOWER);
+                if (timer_elapsed(low_esc_timer) < TAPPING_TERM) {
+                    if (mods & MOD_LALT || mods & MOD_LGUI) {
+                        tap_code(KC_GRAVE);
+                    } else {
+                        tap_code(KC_ESCAPE);
+                    }
+                }
+            }
+            return false;
+        case NUM_TAB:
+            if (record->event.pressed) {
+                num_tab_timer = timer_read();
+                layer_on(_NUMPAD);
+            } else {
+                layer_off(_NUMPAD);
+                if (timer_elapsed(num_tab_timer) < TAPPING_TERM) {
+                    tap_code(KC_TAB);
+                }
+            }
+            return false;
+        case FN_BSPC:
+            if (record->event.pressed) {
+                fn_bspc_timer = timer_read();
+                layer_on(_FUNCTION);
+            } else {
+                layer_off(_FUNCTION);
+                if (timer_elapsed(fn_bspc_timer) < TAPPING_TERM) {
+                    tap_code(KC_BSPACE);
                 }
             }
             return false;
@@ -44,4 +85,35 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return process_record_keymap(keycode, record);
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case H_A:
+        case H_S:
+        // case H_D:
+        case H_F:
+        case H_X:
+        case H_J:
+        // case H_K:
+        case H_L:
+        case H_SCLN:
+        case H_DOT:
+        case LOW_J:
+        case LOW_K:
+        case LOW_L:
+            return TAPPING_TERM + 50;
+
+        case RSE_ENT:
+        case LOW_ESC:
+        case NUM_TAB:
+        case FN_BSPC:
+            return TAPPING_TERM - 50;
+
+        case CTL_SPC:
+            return TAPPING_TERM - 25;
+
+        default:
+            return TAPPING_TERM;
+    }
 }
