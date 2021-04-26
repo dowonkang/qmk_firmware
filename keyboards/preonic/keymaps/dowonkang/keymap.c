96151643e341744b3e2e6fdaebaacfe294d66386
Dowon Kang <dev@dowonkang.name>
2021-04-26 21:54:38 +0900

    Preonic ADJUST layer

--- keyboards/preonic/keymaps/dowonkang/keymap.c
+++ keyboards/preonic/keymaps/dowonkang/keymap.c
@@ -15,6 +15,7 @@
  */
 
 #include "dowonkang.h"
+#include "muse.h"
 
 enum {
     // clang-format off
@@ -35,6 +36,12 @@ void rtr_reset(qk_tap_dance_state_t *state, void *user_data);
 #define RTH RSE
 #define RTL CTL_SPC
 #define RTR TD(RIGHT_THUMB_RIGHT)
+
+// clang-format off
+#define ______ADJUST_RIGHT_1________ XXXXXXX, MUV_IN , XXXXXXX, XXXXXXX, XXXXXXX
+#define ______ADJUST_RIGHT_2________ MU_ON  , MU_MOD , AU_ON  , MI_ON  , XXXXXXX
+#define ______ADJUST_RIGHT_3________ MU_OFF , MUV_DE , AU_OFF , MI_OFF , XXXXXXX
+// clang-format on
 
 // Keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -79,21 +86,100 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXX, XXXX, ____, ____, ____,    XXXX,    XXXX, XXXX, ____, XXXX, XXXX
     ),
 
+    [_MOUSE] = LAYOUT_wrapper(
+        ____, XXXXXXXXXXXXXXXXXXXXXXXXXXXX, XXXXXXXXXXXXXXXXXXXXXXXXXXXX, XXXX,
+        ______MOUSE_WHEEL_1_________, XXXX, XXXX, ______MOUSE_CURSOR_1________,
+        ______MOUSE_WHEEL_2_________, XXXX, XXXX, ______MOUSE_CURSOR_2________,
+        SFTL, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, ACL0, ACL1, ACL2, XXXX,
+        XXXX, XXXX, ALT , GUI , CTRL,    XXXX,    BTN3, BTN1, BTN2, XXXX, XXXX
+    ),
+
     [_ADJUST] = LAYOUT_wrapper(
         ____, XXXXXXXXXXXXXXXXXXXXXXXXXXXX, XXXXXXXXXXXXXXXXXXXXXXXXXXXX, XXXX,
-        ______FN_LEFT_1_____________, XXXX, XXXX, ______FN_RIGHT_1____________,
-        ______FN_LEFT_2_____________, XXXX, XXXX, ______MOUSE_VI_CURSOR_______,
-        ______FN_LEFT_3_____________, XXXX, XXXX, ______MOUSE_VI_WHEEL________,
+        ______FN_LEFT_1_____________, XXXX, XXXX, ______ADJUST_RIGHT_1________,
+        ______FN_LEFT_2_____________, XXXX, XXXX, ______ADJUST_RIGHT_2________,
+        ______FN_LEFT_3_____________, XXXX, XXXX, ______ADJUST_RIGHT_3________,
         XXXX, XXXX, XXXX, ____, XXXX,    XXXX,    XXXX, ____, XXXX, XXXX, XXXX
     ),
     // clang-format on
 };
 
+bool     muse_mode      = false;
+uint8_t  last_muse_note = 0;
+uint16_t muse_counter   = 0;
+uint8_t  muse_offset    = 70;
+uint16_t muse_tempo     = 50;
+
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (clockwise) {
-        tap_code(KC_VOLU);
+    if (muse_mode) {
+        if (IS_LAYER_ON(_RAISE)) {
+            if (clockwise) {
+                muse_offset++;
+            } else {
+                muse_offset--;
+            }
+        } else {
+            if (clockwise) {
+                muse_tempo += 1;
+            } else {
+                muse_tempo -= 1;
+            }
+        }
     } else {
-        tap_code(KC_VOLD);
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+}
+
+void dip_switch_update_user(uint8_t index, bool active) {
+    switch (index) {
+        case 0:
+            if (active) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            break;
+        case 1:
+            if (active) {
+                muse_mode = true;
+            } else {
+                muse_mode = false;
+            }
+    }
+}
+
+void matrix_scan_user(void) {
+#ifdef AUDIO_ENABLE
+    if (muse_mode) {
+        if (muse_counter == 0) {
+            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+            if (muse_note != last_muse_note) {
+                stop_note(compute_freq_for_midi_note(last_muse_note));
+                play_note(compute_freq_for_midi_note(muse_note), 0xF);
+                last_muse_note = muse_note;
+            }
+        }
+        muse_counter = (muse_counter + 1) % muse_tempo;
+    } else {
+        if (muse_counter) {
+            stop_all_notes();
+            muse_counter = 0;
+        }
+    }
+#endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+    switch (keycode) {
+        case RSE:
+        case LOW:
+            return false;
+        default:
+            return true;
     }
 }
 
--- keyboards/preonic/keymaps/dowonkang/rules.mk
+++ keyboards/preonic/keymaps/dowonkang/rules.mk
@@ -1,3 +1,4 @@
 SRC += muse.c
 LAYOUTS = preonic_1x2uC
 RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = yes
