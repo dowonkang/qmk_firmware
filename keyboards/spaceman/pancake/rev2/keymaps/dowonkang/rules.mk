bcf76b45478ef03a0e58f610af3937e0b604ad74
Dowon Kang <dev@dowonkang.name>
2022-10-05 11:02:40 +0900

    enable steno

--- keyboards/spaceman/pancake/rev2/keymaps/dowonkang/rules.mk
+++ keyboards/spaceman/pancake/rev2/keymaps/dowonkang/rules.mk
@@ -4,5 +4,5 @@ COMMAND_ENABLE = no         # Commands for debug and configuration
 
 # Stenography
 # EXTRAKEY_ENABLE = no
-# STENO_ENABLE = yes
-# STENO_PROTOCOL = geminipr
+STENO_ENABLE = yes
+STENO_PROTOCOL = geminipr
