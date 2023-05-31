58d5354305a0d48fe79b07dea20f731a65b6cc92
Dowon Kang <dev@dowonkang.name>
2023-05-31 12:55:35 +0900

    new keycodes

--- keyboards/spaceman/pancake/rev2/keymaps/dowonkang/keymap.c
+++ keyboards/spaceman/pancake/rev2/keymaps/dowonkang/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUMPAD] = LAYOUT_ortho_4x12(
         // clang-format off
-        _______, _______, KC_HOME, KC_UP  , KC_END , KC_VOLU, KC_NLCK, KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______,
+        _______, _______, KC_HOME, KC_UP  , KC_END , KC_VOLU, KC_NUM , KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______,
         _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_PAST, KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_PENT,
         _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSLS, KC_P1  , KC_P2  , KC_P3  , KC_PEQL, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_P0  , KC_P0  , KC_PDOT, _______, _______
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN] = LAYOUT_ortho_4x12(
         // clang-format off
         _______, _______, WBCK   , WFWD   , WTOP   , SCRSH_A, KC_PSCR, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______,
-        _______, _______, _______, _______, WBTM   , SCRSH_P, KC_SLCK, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
+        _______, _______, _______, _______, WBTM   , SCRSH_P, KC_SCRL, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______,
         _______, _______, _______, _______, _______, _______, KC_PAUS, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
         // clang-format on
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_ortho_4x12(
         // clang-format off
-        RESET  , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         PLOVER , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
